05acd5c85cad60e729e4d5c37d38bfa2393cbfc1
jacopo-derrico <derricoj.design@gmail.com>
2024-01-13 13:40:00 +0100

    jsnack 4 complete

--- jsnack 4/main.js
+++ jsnack 4/main.js
@@ -1,1 +1,9 @@
 //Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+let insertNumber = parseInt(prompt("Inserisci un numero"))
+let result = document.getElementById('result')
+
+if (insertNumber % 2 === 0) {
+    result.innerHTML = insertNumber
+} else {
+    result.innerHTML = insertNumber + 1
+}
