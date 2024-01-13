812eda93098cdfd2ff40f172f7db53ec7bc0014d
jacopo-derrico <derricoj.design@gmail.com>
2024-01-13 12:56:14 +0100

    jsnack 2 complete

--- jsnack 2/main.js
+++ jsnack 2/main.js
@@ -1,1 +1,26 @@
 //Genera un numero casuale tra 1 e 100 e poi chiedi all'utente di indovinare il numero. Il programma poi deve far comparire un messaggio "il numero è troppo alto" oppure "il numero è troppo basso" nel momento in cui il numero detto dall'utente non è perfettamente uguale al numero estratto in modo casuale
+
+function randomizer(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+let randomNumber = randomizer(1, 100)
+console.log(randomNumber)
+let inputNumberHTML = 0
+
+while (inputNumberHTML !== randomNumber) {
+    inputNumberHTML = parseInt(prompt("Indovina il numero"))
+
+    if (inputNumberHTML < randomNumber) {
+        console.log(inputNumberHTML + ": il numero scelto è più basso")
+        alert(inputNumberHTML + ": il numero scelto è più basso")
+    } else if (inputNumberHTML > randomNumber) {
+        console.log(inputNumberHTML + ": il numero scelto è più alto")
+        alert(inputNumberHTML + ": il numero scelto è più alto")
+    } else {
+        console.log(inputNumberHTML + ": il numero è giusto!")
+        alert(inputNumberHTML + ": il numero scelto è giusto!")
+    }
+
+    inputNumberHTML === randomNumber
+}
